a6aa098eac70503883a68e2ce57d5429b0dd53a1
huntercodes <85328038+huntercodes@users.noreply.github.com>
2021-12-04 19:16:20 -0600

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by hunter downey on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
